324d37a71bb3f22fc019d36186790ffb0fa7ad0b
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-08-17 14:43:42 +0800

    Update v2ray.c

--- v2ray.c
+++ v2ray.c
@@ -77,9 +77,7 @@ Menu:UI();
     }
     else if (mode == 6) {
         printf("正在更新xray主程序. . .\n");
-        system("systemctl stop xray");
-        system("curl -sSL https://cdn.jsdelivr.net/gh/XTLS/Xray-install/install-release.sh | sh");
-        system("systemctl start xray");
+        system("bash -c \"$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)\" @ install");
         printf("xray主程序更新完成！\n");
         printf("正在检测xray运行状态，以下输出不为空则运行正常！\n");
         printf("--------------以下输出不为空则xray运行正常------------------\n");
@@ -121,7 +119,7 @@ int install_xray() {
     system("setenforce 0");
     system("yum install -y curl uuidgen bind-utils qrencode");
     printf("正在运行xray安装脚本. . .\n");
-    system("curl -sSL https://cdn.jsdelivr.net/gh/XTLS/Xray-install/install-release.sh | sh");
+    system("bash -c \"$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)\" @ install");
     system("rm -rf TCPO.sh");
     printf("正在复制SSL证书与私钥. . .\n");
     system("cp -rf /root/1.pem /usr/local/etc/xray/certificate.pem");
