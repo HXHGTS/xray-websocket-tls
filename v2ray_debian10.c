58223e9d7ca2a89df3db05c480ed7f7b31e26c70
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-11-16 23:31:36 +0800

    Update v2ray_debian10.c

--- v2ray_debian10.c
+++ v2ray_debian10.c
@@ -189,6 +189,7 @@ int KernelUpdate() {
         if (fopen("/root/1.pem", "r") == NULL || fopen("/root/2.pem", "r") == NULL) {
         printf("检测到证书与私钥文件未按照规定方式放置于根目录，强制退出！\n");
         exit(0);
+        }
         printf("请输入已绑定此服务器ip的域名:");
         scanf("%s", sni);
         config = fopen("/usr/local/etc/sni.conf", "w");
